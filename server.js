58d7c35ca433198968c42575e20f402e05d1d9d7
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-01-10 20:40:09 -0600

    completes simple web server ICA

--- server.js
+++ server.js
@@ -1,19 +1,66 @@
 var http = require("http");
+var url = require("url");
 
-var server7000 = http.createServer(function(request, response) {
-    response.end("You don't suck");
+var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:%s", PORT);
 });
 
-var server7500 = http.createServer(function(request, response) {
-    response.end("You suck");
-});
+function handleRequest(req, res) {
 
+    var urlParts = url.parse(req.url);
 
+    switch (urlParts.pathname) {
+        case "/":
+            home(urlParts.pathname, req, res);
+            break;
+        case "/favoritefood":
+            favoriteFood(urlParts.pathname, req, res);
+            break;
+        case "/favoritemovies":
+            favoriteMovies(urlParts.pathname, req, res);
+            break;
+        case "/favoritecss":
+            favoriteCss(urlParts.pathname, req, res);
+            break;
+        default:
+            display404(urlParts.pathname, req, res);
+    }
+}
 
-server7000.listen(7000, function() {
-    console.log("Server listening on port 7000");
-});
+function home(url, req, res) {
+    var html = "<html>";
+    html += "<body><h1>Home Page</h1></body></html>";
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+}
 
-server7500.listen(7500, function() {
-    console.log("Server listening on port 7500");
-});
+function favoriteFood(url, req, res) {
+    var html = "<html>";
+    html += "<body><h1>Favorite Foods</h1><a href='/'>Home</a></body></html>";
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+}
+
+function favoriteMovies(url, req, res) {
+    var html = "<html>";
+    html += "<body><h1>Favorite Movies</h1><a href='/'>Home</a></body></html>";
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+}
+
+function favoriteCss(url, req, res) {
+    var html = "<html>";
+    html += "<body><h1>Favorite CSS</h1><a href='/'>Home</a></body></html>";
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+}
+
+function display404(url, req, res) {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h1>404 Not Found </h1>");
+    res.end("The page you were looking for: " + url + " can not be found ");
+}
